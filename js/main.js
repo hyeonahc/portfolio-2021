60310c061018b092ae3f8b2a95886dfdd7320b8e
hyeonah <hyeonah.hello@gmail.com>
2022-04-17 21:31:14 +0900

    feat: Create menu, section and add scroll to section feature

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// variable declaration
+const menus = document.querySelectorAll('.main-menu a');
+const sections = document.querySelectorAll('section');
+let secOffsetTop;
+let oneWheel;
+
+// window starts from position(0, 0) wherever it's loaded
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+// when each menu is clicked
+// 1. execute 'on' function
+// 2. scroll to section
+menus.forEach((menu, index) => {
+  menu.addEventListener('click', function (e) {
+    e.preventDefault;
+    on(menu);
+    secOffsetTop = sections[index].offsetTop;
+    window.scrollTo({
+      top: secOffsetTop,
+      behavior: 'smooth',
+    });
+  });
+});
+
+// class 'on' will be removed from the menu
+function clearOn() {
+  menus.forEach(menu => {
+    menu.classList.remove('on');
+  });
+}
+
+// class 'on' will be added to the menu
+function on(menu) {
+  console.log(menu);
+  clearOn();
+  menu.classList.add('on');
+}
+
+// When user scrolls to the section, the menu will have 'on' class accordingly
+window.addEventListener('scroll', function () {
+  clearTimeout(oneWheel);
+  oneWheel = setTimeout(function () {
+    const scrolled = window.scrollY;
+    const innerHeight = window.innerHeight;
+    console.log(scrolled, innerHeight);
+    if (scrolled === 0) {
+      on(menus[0]);
+    }
+    if (scrolled > innerHeight / 2) {
+      on(menus[1]);
+    }
+    if (scrolled > innerHeight + innerHeight / 2) {
+      on(menus[2]);
+    }
+    if (scrolled > innerHeight * 2 + innerHeight / 2) {
+      on(menus[3]);
+    }
+    if (scrolled > innerHeight * 3 + innerHeight / 2) {
+      on(menus[4]);
+    }
+  }, 80);
+});
